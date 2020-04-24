8fb3188111b0c4c53ba1c7f0de5beb53df0c0e55
Roman Kozlov <rkozlov95@mail.ru>
2020-04-24 14:56:21 +0300

    update welcome template, add ticket model, ticket controller

--- app/Ticket.php
+++ app/Ticket.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
-    //
+    protected $fillable = [
+    'user_id', 'ticket_id', 'subject', 'priority', 'message', 'status'
+    ];
 }
--- database/migrations/2020_04_23_135220_create_tickets_table.php
+++ database/migrations/2020_04_23_135220_create_tickets_table.php
@@ -15,6 +15,11 @@ class CreateTicketsTable extends Migration
     {
         Schema::create('tickets', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id')->unsigned();
+            $table->string('ticket_id')->unique();
+            $table->string('subject');
+            $table->text('message');
+            $table->string('status');
             $table->timestamps();
         });
     }
